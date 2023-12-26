ecea5f912425ae29e85ca22174a0eeb341de690d
Vívian Oliveira <viviancol2@yahoo.com.br>
2023-12-26 15:35:25 -0300

    feat: arquivo route teste

--- challenge2group1/src/router/teste.jsx
+++ challenge2group1/src/router/teste.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
 	return <nav>
 		<Link to="/">Home</Link>
-		<Link to="/About">About</Link>
+		<Link to="/About">About us</Link>
 	</nav>;
 
 };
