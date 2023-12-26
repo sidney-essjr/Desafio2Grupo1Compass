04850ad68628e41d5d5dafc7bb2278f5dc94f0ae
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2023-12-26 17:35:19 -0300

    Feat: adding the router provider

--- challenge2group1/src/app.jsx
+++ challenge2group1/src/app.jsx
@@ -1,10 +1,11 @@
-import Header from "./components/Header";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 
 export function App() {
-
+  
   return (
     <>
-    <Header/>
+     <RouterProvider router={router}/>
     </>
   )
 }
