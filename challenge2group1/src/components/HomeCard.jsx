9de2642c9f7845d1450398aa28047e06fd469c65
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2023-12-27 20:04:52 -0300

    Refactor: deleting the wrong file

--- challenge2group1/src/components/HomeCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function HomeCard({image, imageDescription, title, text}){
-  return(
-    <section>
-      <img src={image} alt={imageDescription} />
-      <h2>{title}</h2>
-      <p>{text}</p>
-    </section>
-  )
-}
