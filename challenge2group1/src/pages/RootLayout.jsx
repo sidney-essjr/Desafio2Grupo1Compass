08d2f3a69680d221666b14d76293b31accd8a4e1
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2023-12-27 14:30:44 -0300

    Refactor: adding footer on rootlayout page

--- challenge2group1/src/pages/RootLayout.jsx
+++ challenge2group1/src/pages/RootLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer"
 
 export default function RootLayout(){
   return (
     <>
      <Header/>
      <Outlet />  
-     {/* <Footer/> */}
+     <Footer/>
     </>
   )
 }
