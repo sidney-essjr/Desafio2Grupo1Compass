649610cc3e08b8f4653cffa3ad9dc17aff5ad365
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2023-12-27 15:31:32 -0300

    Feat: testing pages

---
+++ challenge2group1/src/pages/About.jsx
@@ -0,0 +1,10 @@
+export default function About(){
+  return (
+    <>
+      <h1>Testing About component 1</h1>
+      <h1>Testing About component 2</h1>
+      <h1>Testing About component 3</h1>
+      <h1>Testing About component 4</h1>
+    </>
+  )
+}
---
+++ challenge2group1/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home(){
+  return (
+    <>
+      <h1>Testing Home component 1</h1>
+      <h1>Testing Home component 2</h1>
+      <h1>Testing Home component 3</h1>
+      <h1>Testing Home component 4</h1>
+    </>
+  )
+}
