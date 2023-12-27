625f96ae20fa564b10b2bfff3047fcb2ff3343b2
Vívian Oliveira <viviancol2@yahoo.com.br>
2023-12-26 22:11:26 -0300

    update: correção de falhas no arquivo app.jsx

--- challenge2group1/src/app.jsx
+++ challenge2group1/src/app.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -7,7 +6,10 @@ export function App() {
     <>
       <Header />
       <Footer />
-=======
+    </>
+);
+}
+
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 
@@ -16,7 +18,6 @@ export function App() {
   return (
     <>
      <RouterProvider router={router}/>
->>>>>>> ebc7f4b1aaefce64516610f1ce29745b9eca18bf
     </>
   );
 }
