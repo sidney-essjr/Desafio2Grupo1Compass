ee45449f7171c2d76cf54fd10776ee2df212d2f6
Vívian Oliveira <viviancol2@yahoo.com.br>
2023-12-27 17:43:08 -0300

    Update Products.jsx

--- challenge2group1/src/pages/Products.jsx
+++ challenge2group1/src/pages/Products.jsx
@@ -1,10 +1,10 @@
 export default function Product(){
-  return (
-    <>
-      <h1>Testing Product component 1</h1>
-      <h1>Testing Product component 2</h1>
-      <h1>Testing Product component 3</h1>
-      <h1>Testing Product component 4</h1>
-    </>
-  )
-}
+    return (
+      <>
+        <h1>Testing Product component 1</h1>
+        <h1>Testing Product component 2</h1>
+        <h1>Testing Product component 3</h1>
+        <h1>Testing Product component 4</h1>
+      </>
+    )
+  }
