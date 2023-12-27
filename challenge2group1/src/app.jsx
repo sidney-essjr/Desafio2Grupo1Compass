9c00cbf90b190af8467f3fc95ace5f20e9ff34fa
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2023-12-27 14:24:02 -0300

    Refactor: organizing components on the right file (rootlayout)

--- challenge2group1/src/app.jsx
+++ challenge2group1/src/app.jsx
@@ -1,13 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 export function App() {
   
   return (
     <>
-     <Header />
      <RouterProvider router={router}/>
     </>
   );
