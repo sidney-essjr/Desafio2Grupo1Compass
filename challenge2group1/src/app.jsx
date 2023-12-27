4fc72925dd6acc890487a5faa39c0bec8c9b22d5
sidney.jr <sidney.e.s.s.jr@gmail.com>
2023-12-27 13:44:22 -0300

    fix: includes json-server dependencies

--- challenge2group1/src/app.jsx
+++ challenge2group1/src/app.jsx
@@ -9,10 +9,6 @@ export function App() {
     <>
      <Header />
      <RouterProvider router={router}/>
-<<<<<<< HEAD
-=======
-     <Footer />
->>>>>>> 58fcbc4e59af9906989cc0dbf04857dff3d0e0f7
     </>
   );
 }
