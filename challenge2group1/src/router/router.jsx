cddf8edfe9cd6aac628eea68eb9194be61eabaf6
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2023-12-27 15:28:30 -0300

    Feat: adding pages to outlet component

--- challenge2group1/src/router/router.jsx
+++ challenge2group1/src/router/router.jsx
@@ -1,21 +1,21 @@
 import { createBrowserRouter } from "react-router-dom"
 import RootLayout from "../pages/RootLayout"
-// import {Home} from "../pages/Home"
-// import {RegistrationForm} from "../pages/RegistrationForm"
-// import {Products} from "../pages/Products"
-// import {About} from "../pages/About"
+import Home from "../pages/Home"
+import RegistrationForm from "../pages/RegistrationForm"
+import Products from "../pages/Products"
+import About from "../pages/About"
 
 
 const router = createBrowserRouter([
 	{
     path: "/",
     element: <RootLayout />,
-    // children: [
-      // {index: true, element: <Home />}, 
-      // {path: "register", element: <Register/>},
-      // {path: "products", element: <Products />},
-      // {path: "about", element: <About />}
-    // ]
+    children: [
+      {index: true, element: <Home />}, 
+      {path: "registrationForm", element: <RegistrationForm/>},
+      {path: "products", element: <Products />},
+      {path: "about", element: <About />}
+    ]
   }
 ]) 
 export default router
