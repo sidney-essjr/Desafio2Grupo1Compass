1f394f156994ac1a9431b4616a1d9a53ada4e0e3
Vívian Oliveira <viviancol2@yahoo.com.br>
2023-12-28 15:41:07 -0300

    Update Product.jsx

--- challenge2group1/src/components/Product.jsx
+++ challenge2group1/src/components/Product.jsx
@@ -1,7 +1,7 @@
 export function Product({imgUrl, name, subtitle, label, price, features, description}) {
     return (
     <li>
-        <img src={`http://localhost:3000/plants ${imgUrl}`} />
+        <img src={`src/${imgUrl}`} />
         <h1>{name}</h1>
         <h2>{subtitle}</h2>
         <p>{label}</p>
