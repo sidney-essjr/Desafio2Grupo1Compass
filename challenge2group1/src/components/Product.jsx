e7548ddf48de19376868212afdc890096e7a204e
Vívian Oliveira <viviancol2@yahoo.com.br>
2023-12-28 14:27:00 -0300

    update: img page pruducts

--- challenge2group1/src/components/Product.jsx
+++ challenge2group1/src/components/Product.jsx
@@ -1,13 +1,13 @@
 export function Product({imgUrl, name, subtitle, label, price, features, description}) {
     return (
     <li>
+        <img src={`http://localhost:3000/plants ${imgUrl}`} />
         <h1>{name}</h1>
         <h2>{subtitle}</h2>
         <p>{label}</p>
         <p>{price}</p>
         <p>{features}</p>
         <p>{description}</p>
-        <img src={imgUrl} alt={name} />
     </li>
     );
 }
