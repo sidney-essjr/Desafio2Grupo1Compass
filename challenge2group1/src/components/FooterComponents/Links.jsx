72af13ee668c5834d078a5a42d7f2104a03ab8b7
will.terra <wpterra@outlook.com>
2024-01-02 17:06:29 -0300

    fix: Footer Responsive margins

--- challenge2group1/src/components/FooterComponents/Links.jsx
+++ challenge2group1/src/components/FooterComponents/Links.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Links = () => {
   return (
-    <ul>
+    <ul className="md:mt-6">
       <label className="font-lato font-bold text-2xl "> Links</label>
-      <li className="mt-6 font-raleway mb-3">About us</li>
-      <li className="font-raleway mb-3">Products</li>
-      <li className="font-raleway mb-3">Blogs</li>
+      <li className="md:mt-6 mt-2 font-raleway mb-1 md:mb-3">About us</li>
+      <li className="font-raleway mb-1 md:mb-3">Products</li>
+      <li className="font-raleway ">Blogs</li>
     </ul>
   );
 };
