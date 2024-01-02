19ce9b4b00ea3adaa959f836138c3869b9301db0
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2024-01-02 16:17:06 -0300

    Feat: adding initial styles

--- challenge2group1/src/components/AboutUsComponent/CardAboutUs.jsx
+++ challenge2group1/src/components/AboutUsComponent/CardAboutUs.jsx
@@ -1,9 +1,22 @@
 export default function CardAboutUs({dev}){
   return (
-    <section key={dev.id}>
-      <img src={`src/${dev.imgUrl}`} alt=" " />
-      <p>{dev.name}, {dev.idade}, {dev.cidade}</p>
-      <p>{dev.aboutMe}</p>
+    <section 
+      key={dev.id}
+      className="w-1/2  mt-10 mb-10 h-96 flex flex-col items-center mx-auto bg-lunar rounded-tl-2xl"
+    >
+      <img 
+        src={`src/${dev.imgUrl}`} 
+        alt=" "
+        className="w-44 rounded-full mt-10"
+      />
+      <div
+        className="mt-6 text-white "
+      >
+        <p
+          className=""
+        >{dev.name}, {dev.idade}, {dev.cidade}</p>
+        <p>{dev.aboutMe}</p>
+      </div>
     </section>
   )
 }
