2a37a37bc70df863cb229d459be31b31d7fdaff6
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2024-01-03 15:29:43 -0300

    Fix: adding custom theme on the right place

--- challenge2group1/tailwind.config.js
+++ challenge2group1/tailwind.config.js
@@ -1,34 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      gelo: "#F0F1EE",
-      abacate: "#8A9B6E",
-      lunar: "#354733",
-      white: "#ffffff",
-      accent: "#DDDEA0",
-      linegray: "#AFB2AA",
-      textgray: "#808276",
-      errorred: "#e20000",
-      verdinho: "#83E281",
+  theme: {  
+    extend: {
+      colors: {
+        gelo: "#F0F1EE",
+        abacate: "#8A9B6E",
+        lunar: "#354733",
+        white: "#ffffff",
+        accent: "#DDDEA0",
+        linegray: "#AFB2AA",
+        textgray: "#808276",
+        errorred: "#e20000",
+        verdinho: "#83E281",
+      },
+      fontFamily: {
+        raleway: ['"Raleway"'],
+        garamond: ['"EB Garamond"'],
+        lato: ['"Lato"'],
+        inter: ['"Inter"'],
+        pacifico: ['"Pacifico"'],
+      },
+      screens: {
+        md: "767px",
+      },
+  
+      borderRadius: {
+        borderFull: "50%",
+      },
+      
     },
-    fontFamily: {
-      raleway: ['"Raleway"'],
-      garamond: ['"EB Garamond"'],
-      lato: ['"Lato"'],
-      inter: ['"Inter"'],
-      pacifico: ['"Pacifico"'],
-    },
-    screens: {
-      md: "767px",
-    },
-
-    borderRadius: {
-      borderFull: "50%",
-    },
-    
-    extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
 };
