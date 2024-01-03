837dc82c8bc77796a9d88c5bd85813d52816c85f
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-03 00:23:16 -0300

    update: ProductDetails.jsx

--- challenge2group1/src/components/ProductComponents/ProductDetail.jsx
+++ challenge2group1/src/components/ProductComponents/ProductDetail.jsx
@@ -11,18 +11,22 @@ export const ProductDetail = () => {
       src={`../src/${plant.imgUrl}`} /></div>
       </div>
       <div className="md:w-1/2 p-8">
-      <h1 className="text-3xl font-bold mb-4">{plant.name}</h1>
-      <h2 className="text-lg text-gray-600 mb-4">{plant.subtitle}</h2>
-      <p className="text-green-500 font-semibold mb-4">{plant.label}</p>
-      <p className="text-2xl font-bold mb-4">{plant.price}</p>
-      <p>
-        <a target="blank"
-          href={`https://www.google.com/search?q=comprar+${plant.name}`} >
-          Check Out
-        </a>
-      </p>
-      <ul className="list-disc pl-6 mb-4">{plant.features}</ul>
-      <p className="text-base mb-4">{plant.description}</p>
+      <h1 className="text-lunar text-[64px] font-bold font-garamond">{plant.name}</h1>
+      <h2 className="pstyle text-[24px]">{plant.subtitle}</h2>
+      <p className="text-verdinho border border-verdinho rounded-full px-3 py-1 font-semibold mr-2">{plant.label}</p>
+      <p className="text-lunar text-[20px] rounded-full px-3 py-1 font-semibold">{plant.price}</p>
+      <div>
+        <a className="flex justify-center w-191 h-62 gap-10 bg-lunar text-white px-6 py-4" target="blank"
+          href={`https://www.google.com/search?q=comprar+${plant.name}`} > Check Out </a>
+     </div>
+      <p className="text-black text-[24px] font-bold font-lato" >Features</p>
+      <ul className="list-disc pl-6 mb-4 text-black text-[16px] font-raleway">
+      {plant.features.map((feature, index) => (
+      <li key={index}>{feature}</li>
+    ))}
+      </ul>
+      <p className="text-black text-[24px] font-bold font-lato">Discription</p>
+      <p className="text-base mb-4 text-black text-[16px] font-raleway">{plant.description}</p>
       </div>
     </li>
   );
