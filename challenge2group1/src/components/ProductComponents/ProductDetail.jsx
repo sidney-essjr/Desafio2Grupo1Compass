ba62d696ddc383dc352bc603125f28c2834f0fc8
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-03 12:15:03 -0300

    Update ProductDetail.jsx

--- challenge2group1/src/components/ProductComponents/ProductDetail.jsx
+++ challenge2group1/src/components/ProductComponents/ProductDetail.jsx
@@ -6,14 +6,14 @@ export const ProductDetail = () => {
   return (
     
     <li className="flex flex-col md:flex-row items-center h-screen">
-      <div className="md:w-1/2 w-96 h-96">
-      <img className="flex-shrink-0 opacity-90 bg-cover-repeat bg-center" 
+      <div className="md:w-1/2">
+      <img className="w-[752px] h-[690px] first-line:flex-shrink-0 opacity-90 bg-cover-repeat bg-center" 
       src={`../src/${plant.imgUrl}`} /></div>
 
       <div className="md:w-1/2 p-8">
       <h1 className="text-lunar text-[64px] font-bold font-garamond">{plant.name}</h1>
       <h2 className="pstyle text-[24px]">{plant.subtitle}</h2>
-      <p className="text-verdinho border border-verdinho rounded-full px-3 py-1 font-semibold mr-2">{plant.label}</p>
+      <p className="text-verdinho border border-verdinho rounded-[100px] px-3 py-1 font-semibold mr-2">{plant.label.join(' ')}</p>
       <p className="text-lunar text-[20px] rounded-full px-3 py-1 font-semibold">{plant.price}</p>
       <div>
         <a className="flex justify-center w-191 h-62 gap-10 bg-lunar text-white px-6 py-4" target="blank"
