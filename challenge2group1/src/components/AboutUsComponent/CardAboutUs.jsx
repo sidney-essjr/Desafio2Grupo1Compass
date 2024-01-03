965ed2737eace76b6e0010a11f5a844e6480a9d1
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-02 22:47:37 -0300

    update

--- challenge2group1/src/components/AboutUsComponent/CardAboutUs.jsx
+++ challenge2group1/src/components/AboutUsComponent/CardAboutUs.jsx
@@ -1,20 +1,9 @@
 export default function CardAboutUs({dev}){
   return (
-    <section 
-      key={dev.id}
-      className="w-1/2  mt-10 mb-10 h-96 flex flex-col items-center mx-auto bg-lunar rounded-tl-2xl"
-    >
-      <img 
-        src={`src/${dev.imgUrl}`} 
-        alt=" "
-        className="w-44 rounded-full mt-10"
-      />
-      <div
-        className="mt-6 text-white "
-      >
-        <p
-          className=""
-        >{dev.name}, {dev.idade}, {dev.cidade}</p>
+    <section className="w-1/2 mt-10 mb-10 h-96 flex flex-col items-center mx-auto bg-lunar rounded-tl-2xl" key={dev.id} >
+      <img className="w-44 rounded-full mt-10" src={`src/${dev.imgUrl}`} alt=" " />
+      <div className="mt-6 text-white ">
+        <p className=" ">{dev.name}, {dev.idade}, {dev.cidade}</p>
         <p>{dev.aboutMe}</p>
       </div>
     </section>
