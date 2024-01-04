779c7e682b19e3bd5e8cf3265f8e1f027680b092
Vívian Oliveira <viviancol2@yahoo.com.br>
2024-01-04 14:32:34 -0300

    update: love for nature - in home

--- challenge2group1/src/pages/Home.jsx
+++ challenge2group1/src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
           <div className="flex w-screen justify-center md:px-20 px-5 pt-5  md:pt-20  bg-gelo">
             <div className="">
               <div className="inline-flex justify-start align-bottom">
-                <img src="rectangle.svg" alt="a" />
+                <img className="ml-6" src="rectangle.svg" alt="a" />
                 <p className=" font-pacifico ml-2">love for nature</p>{" "}
               </div>
               <div className="w-full">
